8e7e660458331524a5596c631966fe1f196e2c5a
Julien Piron <tech@jpiron.eu>
2021-12-04 21:09:49 +0100

    style(Wordcounter): basic style

--- components/App.tsx
+++ components/App.tsx
@@ -3,8 +3,10 @@ import Wordcounter from "./Wordcounter"
 
 const App: FC = () => {
   return (
-    <main>
-      <Wordcounter />
+    <main className="min-h-screen p-16 bg-blue-500">
+      <div className="bg-white p-8 rounded grid grid-cols-6 gap-4">
+        <Wordcounter />
+      </div>
     </main>
   )
 }
--- components/Wordcounter.tsx
+++ components/Wordcounter.tsx
@@ -5,12 +5,13 @@ const Wordcounter: FC = () => {
   return (
     <>
       <input
+        className="col-span-5 focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50"
         type="text"
         aria-label="Text you want to count the caracters within"
         value={value}
         onChange={(event) => setValue(event.target.value)}
       />
-      <p>{value.length}</p>
+      <p className="col-span-1 text-xl font-bold">{value.length}</p>
     </>
   )
 }
