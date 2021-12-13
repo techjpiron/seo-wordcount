83be6f91cc57db3d703cd24a906608e16595c363
Julien Piron <julien.piron@outlook.com>
2021-12-13 16:20:25 +0100

    chore: add page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
+import Head from "next/head"
 import type { NextPage } from "next"
 import App from "../components/App"
 
 const Home: NextPage = () => {
-  return <App />
+  return (
+    <>
+      <Head>
+        <title>SEO Wordcounter | Julien Piron</title>
+      </Head>
+      <App />
+    </>
+  )
 }
 
 export default Home
